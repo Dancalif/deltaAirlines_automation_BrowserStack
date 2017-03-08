e0a5c0ef592c53da40e5821950ee2f6ed7c4b445
Denis Umanets <dan@Deniss-MacBook-Pro.local>
2017-03-08 00:29:07 -0800

    BrowserStack implementation

--- src/test/java/com/deltaAirlines/delta_automation/Main_Test.java
+++ src/test/java/com/deltaAirlines/delta_automation/Main_Test.java
@@ -1,40 +1,79 @@
 package com.deltaAirlines.delta_automation;
 
-import java.util.concurrent.TimeUnit;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.browserstack.local.Local;
 
 public class Main_Test {
-	WebDriver driver;
+	public WebDriver driver;
+	private Local l;
 
-	@Parameters({ "browser", "url" })
-	@BeforeTest
-	public void getWebDriver(String browser, String url) {
+	@BeforeMethod(alwaysRun = true)
+	@org.testng.annotations.Parameters(value = { "conf", "environment", "url" })
+	public void setUp(String conf, String environment, String url) throws Exception {
+		JSONParser parser = new JSONParser();
+		JSONObject config = (JSONObject) parser.parse(new FileReader(
+				"/Users/dan/Dropbox/Delta_workspace/deltaAirlines_automation_BrowserStack/src/test/java/resources/"
+						+ conf));
+		JSONObject envs = (JSONObject) config.get("environments");
 
-		// Launch the FireFox browser
-		if (browser.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-		} else if (browser.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver",
-					"/Users/dan/Dropbox/Delta_workspace/delta-automation/webDrivers/chromedriver");
-			driver = new ChromeDriver();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+
+		Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
+		Iterator it = envCapabilities.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
 		}
-		// Navigate to AUT, managing the browser window
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get("https://google.com");
+
+		Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+		it = commonCapabilities.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			if (capabilities.getCapability(pair.getKey().toString()) == null) {
+				capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+			}
+		}
+
+		String username = System.getenv("BROWSERSTACK_USERNAME");
+		if (username == null) {
+			username = (String) config.get("user");
+		}
+
+		String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+		if (accessKey == null) {
+			accessKey = (String) config.get("key");
+		}
+
+		if (capabilities.getCapability("browserstack.local") != null
+				&& capabilities.getCapability("browserstack.local") == "true") {
+			l = new Local();
+			Map<String, String> options = new HashMap<String, String>();
+			options.put("key", accessKey);
+			l.start(options);
+		}
+
+		driver = new RemoteWebDriver(
+				new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
 		driver.get(url);
 	}
 
-	@AfterTest
-	public void tearDown() throws InterruptedException {
-		// Clean up environment
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() throws Exception {
 		driver.quit();
+		if (l != null)
+			l.stop();
 	}
 }
